b15b0a1041f7fc762bef1cb2fd48607e9acc3cd5
RuanPetrus <xastroboyx11@gmail.com>
2025-06-06 12:43:59 -0300

    Implemented warp tiling

--- gemm.cu
+++ gemm.cu
@@ -3,7 +3,10 @@
 
 #define ERROR(message, ...) do { fprintf(stderr, message, ##__VA_ARGS__); abort(); } while(0)
 
-template<const uint BN, const uint BM, const uint BK, 
+#define WARP_SIZE 32
+
+template<const uint BN, const uint BM, const uint BK,
+		 const uint WN, const uint WM, const uint WMITER,
          const uint TN, const uint TM, const uint NUM_THREADS>
 __global__ void __launch_bounds__(NUM_THREADS)
 	kernel_gemm(uint N, uint M, uint K, 
@@ -11,9 +14,6 @@ __global__ void __launch_bounds__(NUM_THREADS)
 {
 	const uint blrow = blockIdx.y;
 	const uint blcol = blockIdx.x; 
-
-	const uint trow  = threadIdx.x / (BM / TM);
-	const uint tcol  = threadIdx.x % (BM / TM);
 
 	const uint iArow = threadIdx.x / (BK / 4);
 	const uint iAcol = threadIdx.x % (BK / 4);
@@ -23,14 +23,26 @@ __global__ void __launch_bounds__(NUM_THREADS)
 	const uint iBcol = threadIdx.x % (BM / 4);
 	constexpr uint strideB = NUM_THREADS / (BM / 4);
 
+	const uint widx = threadIdx.x / WARP_SIZE;
+	const uint wrow = widx / (BM / WM);
+	const uint wcol = widx % (BM / WM);
+
+	constexpr uint WNITER = (WN*WM) / (WARP_SIZE*TN*TM*WMITER);
+	constexpr uint SUBWN = WN / WNITER;
+	constexpr uint SUBWM = WM / WMITER;
+
+	const uint tidx = threadIdx.x % WARP_SIZE;
+	const uint trow = tidx / (SUBWM / TM);
+	const uint tcol = tidx % (SUBWM / TM);
+
 	__shared__ float SA[BN*BK], SB[BK*BM];
 
 	A += (blrow * BN)*K;
 	B += (blcol * BM)*1;
-	C += (blrow * BN)*M + (blcol * BM)*1;
+	C += (blrow * BN + wrow * WN)*M + (blcol * BM + wcol *WM)*1;
 	
-	float rs[TN*TM] = {0};
-	float regA[TN], regB[TM];
+	float rs[WNITER*WMITER*TN*TM] = {0}; // [WNITER][WMITER][TN][TM]
+	float regA[WNITER*TN], regB[WMITER*TM];
 
 	for (uint blit = 0; blit < (K/BK); blit++) {
 		// Loading A and B to shared memory
@@ -53,16 +65,25 @@ __global__ void __launch_bounds__(NUM_THREADS)
 
 		// Doing computation
 		for (uint k = 0; k < BK; k++) {
-			for (int i = 0; i < TN; i++) {
-				// regA[i] = SA[(trow*TN+i)*BK +k];
-				regA[i] = SA[(k)*BN +(trow*TN+i)];
+			for (uint iSubN = 0; iSubN < WNITER; iSubN++) {
+				for (uint rn = 0; rn < TN; rn++) {
+					regA[iSubN*TN + rn] = SA[k*BN + (wrow*WN + iSubN*SUBWN + trow*TN + rn)];
+				}
 			}
-			for (int i = 0; i < TM; i++) {
-				regB[i] = SB[k*BM + (tcol*TM+i)];
+			for (uint iSubM = 0; iSubM < WMITER; iSubM++) {
+				for (uint rm = 0; rm < TM; rm++) {
+					regB[iSubM*TM + rm] = SB[k*BM + (wcol*WM + iSubM*SUBWM + tcol*TM + rm)];
+				}
 			}
-			for (uint resN = 0; resN < TN; resN++) {
-				for (uint resM = 0; resM < TM; resM++) {
-					rs[resN*TM+resM] += regA[resN] *  regB[resM];
+
+			for (uint iSubN = 0; iSubN < WNITER; iSubN++) {
+				for (uint iSubM = 0; iSubM < WMITER; iSubM++) {
+					for (uint rn = 0; rn < TN; rn++) {
+						for (uint rm = 0; rm < TM; rm++) {
+							rs[iSubN*WMITER*TN*TM + iSubM*TN*TM + rn*TM + rm] += 
+								regA[iSubN*TN + rn] * regB[iSubM*TM + rm];
+						}
+					}
 				}
 			}
 		}
@@ -70,11 +91,17 @@ __global__ void __launch_bounds__(NUM_THREADS)
 		B += BK*M;
 		__syncthreads();
 	}
-	for (uint resN = 0; resN < TN; resN++) {
-		for (uint resM = 0; resM < TM; resM += 4) {
-			float4 tmp = reinterpret_cast<float4 *>(&rs[resN*TN + resM])[0];
-			reinterpret_cast<float4 *>
-				(&C[(trow*TN + resN)*M + (tcol*TM+resM)])[0] = tmp;
+	for (uint iSubN = 0; iSubN < WNITER; iSubN++) {
+		for (uint iSubM = 0; iSubM < WMITER; iSubM++) {
+			for (uint rn = 0; rn < TN; rn++) {
+				for (uint rm = 0; rm < TM; rm += 4) {
+					float4 tmp = reinterpret_cast<float4 *>(&rs[
+						iSubN*WMITER*TN*TM + iSubM*TN*TM + rn*TM + rm]
+					)[0];
+					reinterpret_cast<float4 *>
+						(&C[(iSubN*SUBWN + trow*TN + rn)*M + (iSubM*SUBWM + tcol*TM + rm)])[0] = tmp;
+				}
+			}
 		}
 	}
 }
@@ -83,12 +110,15 @@ __global__ void __launch_bounds__(NUM_THREADS)
 
 void gemm(uint N, uint M, uint K, const float *A, const float *B, float *C) 
 {
+	constexpr uint NUM_THREADS = 128;
 	constexpr uint BN = 128;
-	constexpr uint BM = 128;
+	constexpr uint BM = 64;
 	constexpr uint BK = 16;
-	constexpr uint TN = 8;
-	constexpr uint TM = 8;
-	constexpr uint NUM_THREADS = (BN*BM) / (TN*TM);
+	constexpr uint WN = 64;
+	constexpr uint WM = 32;
+	constexpr uint WMITER = 1;
+	constexpr uint TN = 4;
+	constexpr uint TM = 4;
 
 	static_assert(BM % TM == 0);
 	static_assert(BN % TM == 0);
@@ -102,7 +132,9 @@ void gemm(uint N, uint M, uint K, const float *A, const float *B, float *C)
 	dim3 blockDim(NUM_THREADS);
 	dim3 gridDim(M/BM, N/BN);
 	kernel_gemm
-		<BN, BM, BK, TN, TM, NUM_THREADS>
+		<BN, BM, BK, 
+		WN, WM, WMITER,
+		TN, TM, NUM_THREADS>
 		<<<gridDim, blockDim>>>(N, M, K, A, B, C);
 	cudaError_t err = cudaGetLastError();
 
