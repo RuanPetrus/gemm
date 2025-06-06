9a69fa707db09a26d9b592cbdf17b6ac6baf162d
RuanPetrus <xastroboyx11@gmail.com>
2025-06-06 10:40:48 -0300

    - Added vectorized acess to global memory

--- gemm.cu
+++ gemm.cu
@@ -15,13 +15,13 @@ __global__ void __launch_bounds__(NUM_THREADS)
 	const uint trow  = threadIdx.x / (BM / TM);
 	const uint tcol  = threadIdx.x % (BM / TM);
 
-	const uint iArow = threadIdx.x / BK;
-	const uint iAcol = threadIdx.x % BK;
-	constexpr uint strideA = NUM_THREADS/BK;
+	const uint iArow = threadIdx.x / (BK / 4);
+	const uint iAcol = threadIdx.x % (BK / 4);
+	constexpr uint strideA = NUM_THREADS / (BK / 4);
 	
-	const uint iBrow = threadIdx.x / BM;
-	const uint iBcol = threadIdx.x % BM;
-	constexpr uint strideB = NUM_THREADS/BM;
+	const uint iBrow = threadIdx.x / (BM / 4);
+	const uint iBcol = threadIdx.x % (BM / 4);
+	constexpr uint strideB = NUM_THREADS / (BM / 4);
 
 	__shared__ float SA[BN*BK], SB[BK*BM];
 
@@ -35,11 +35,19 @@ __global__ void __launch_bounds__(NUM_THREADS)
 	for (uint blit = 0; blit < (K/BK); blit++) {
 		// Loading A and B to shared memory
 		for (uint offA = 0; offA < BN; offA += strideA) {
-			// SA[(iArow+offA)*BK + iAcol] = A[(iArow+offA)*K + iAcol];
-			SA[(iAcol)*BN +(iArow+offA)] = A[(iArow+offA)*K + iAcol]; // transposing SA
+			float4 tmp = reinterpret_cast<const float4 *>(&A[(iArow+offA)*K + (iAcol)*4])[0];
+			// Transposing SA
+			SA[(iAcol * 4 + 0)*BN +(iArow+offA)] = tmp.x;
+			SA[(iAcol * 4 + 1)*BN +(iArow+offA)] = tmp.y;
+			SA[(iAcol * 4 + 2)*BN +(iArow+offA)] = tmp.z;
+			SA[(iAcol * 4 + 3)*BN +(iArow+offA)] = tmp.w;
 		}
 		for (uint offB = 0; offB < BK; offB += strideB) {
-			SB[(iBrow+offB)*BM + iBcol] = B[(iBrow+offB)*M + iBcol];
+			float4 tmp = reinterpret_cast<const float4 *>(&B[(iBrow+offB)*M + iBcol*4])[0];
+			SB[(iBrow+offB)*BM + iBcol*4 + 0] = tmp.x;
+			SB[(iBrow+offB)*BM + iBcol*4 + 1] = tmp.y;
+			SB[(iBrow+offB)*BM + iBcol*4 + 2] = tmp.z;
+			SB[(iBrow+offB)*BM + iBcol*4 + 3] = tmp.w;
 		}
 		__syncthreads();
 
@@ -63,8 +71,10 @@ __global__ void __launch_bounds__(NUM_THREADS)
 		__syncthreads();
 	}
 	for (uint resN = 0; resN < TN; resN++) {
-		for (uint resM = 0; resM < TM; resM++) {
-			C[(trow*TN + resN)*M + (tcol*TM+resM)] = rs[resN*TM+resM];
+		for (uint resM = 0; resM < TM; resM += 4) {
+			float4 tmp = reinterpret_cast<float4 *>(&rs[resN*TN + resM])[0];
+			reinterpret_cast<float4 *>
+				(&C[(trow*TN + resN)*M + (tcol*TM+resM)])[0] = tmp;
 		}
 	}
 }
@@ -75,7 +85,7 @@ void gemm(uint N, uint M, uint K, const float *A, const float *B, float *C)
 {
 	constexpr uint BN = 128;
 	constexpr uint BM = 128;
-	constexpr uint BK = 8;
+	constexpr uint BK = 16;
 	constexpr uint TN = 8;
 	constexpr uint TM = 8;
 	constexpr uint NUM_THREADS = (BN*BM) / (TN*TM);
